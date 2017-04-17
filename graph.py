506f3224244023a94f1da2ff20963221b68765d1
liuyujyyz <liuyujyyz@gmail.com>
2017-04-17 17:21:17 +0800

    add backprop

--- graph.py
+++ graph.py
@@ -54,7 +54,16 @@ class graph():
         self.loss_var = self.nodes[net_graph['loss_var']]
         for u in self.nodes:
             self.nodes[u].show()
+    
+    def load(self, filename):
+        data = pickle.load(open(filename, 'rb'))
+        self.nodes = data['nodes']
+        self.edges = data['edges']
+        self.loss_var = data['loss_var']
 
+    def save(self, filename):
+        pickle.dump({'nodes':self.nodes, 'edges':self.edges, 'loss_var':self.loss_var}, open(filename, 'wb'))
+        
     def forward(self, data):
         M = list(self.edges.keys())
         for key in self.nodes:
@@ -71,9 +80,22 @@ class graph():
 
         print(self.loss_var.value)
 
-    def backprop(self):
-        #Not Implemented
-        pass
+    def backprop(self, lr):
+        count = {}
+        for key in self.nodes:
+            self.nodes[key].delta = 0
+            count[key] = 0
+        self.loss_var.delta = self.loss_var.value
+        M = list(self.edges.keys())
+        while M:
+            for opr in M:
+                outname = self.edges[opr].output
+                if count[outname] == len(self.nodes[outname].outputs):
+                    print(opr)
+                    self.edges[opr].bp(self.nodes, lr)
+                    for inname in self.edges[opr].input:
+                        count[inname] += 1
+                    M.remove(opr)
 
 class NetworkBuilder():
     def __init__(self, name = 'HAHA'):
@@ -105,3 +127,14 @@ class NetworkBuilder():
 
     def get_desc(self):
         return {'params':self.params, 'oprs':self.oprs, 'loss_var':self.loss_var, 'time_stamp':self.time_stamp}
+    
+    def load(self, filename):
+        data = pickle.load(open(filename, 'rb'))
+        self.params = data['params']
+        self.oprs = data['oprs']
+        self.loss_var = data['loss_var']
+        self.time_stamp = data['time_stamp']
+
+    def save(self, filename):
+        desc = self.get_desc()
+        pickle.dump(desc, open(filename, 'wb'))
--- test_graph.py
+++ test_graph.py
@@ -2,18 +2,21 @@ import pickle
 from monitor import *
 from dataprovider import *
 from graph import *
+import numpy as np
+
 input_dims = 28 * 28
 hidden_dims = 128
 code_len = 32
-W = pickle.load(open('AEforMNIST/net.pkl','rb'))
-start = W['step']
+#W = pickle.load(open('AEforMNIST/net.pkl','rb'))
+start = 0#W['step']
 
 ins = [input_dims, hidden_dims, code_len, hidden_dims, input_dims]
 
 NB = NetworkBuilder()
 
 for i in range(4):
-    NB.add_opr('fc%s'%i, ['a%s'%i], 'z%s'%i, 'FC', {'inp_size': ins[i], 'out_size':ins[i+1], 'W':W['fc%s:W'%(i+1)], 'b':W['fc%s:b'%(i+1)]})
+#    NB.add_opr('fc%s'%i, ['a%s'%i], 'z%s'%i, 'FC', {'inp_size': ins[i], 'out_size':ins[i+1], 'W':W['fc%s:W'%(i+1)], 'b':W['fc%s:b'%(i+1)]})
+    NB.add_opr('fc%s'%i, ['a%s'%i], 'z%s'%i, 'FC', {'inp_size': ins[i], 'out_size':ins[i+1]})
     NB.add_opr('act%s'%i, ['z%s'%i], 'a%s'%(i+1), 'SIGMOID', {})
 
 NB.add_opr('sub1', ['a4', 'a0'], 'diff', 'SUB', {})
@@ -23,15 +26,19 @@ NB.set_loss_var('sum')
 NB.set_time_stamp(start)
 net = NB.get_desc()
 G = graph(net)
-writer = MonitorWriter('test_graph')
-dp = MNIST(with_label = False)
+#writer = MonitorWriter('test_graph')
+#dp = MNIST(with_label = False)
 
-valid_img = dp.valid()
-G.forward(data = {'a0':valid_img[:100]})
-oup = G.nodes['a4'].value
-canvas = np.concatenate([np.concatenate(p, axis = 0) for p in oup.reshape(10, 10 , 28, 28, 1)], axis = 1)
-writer.add_image('gen_img', canvas, 0)
+#valid_img = dp.valid()
+train_img = np.ones((1024, 28*28))
+for i in range(10):
+    G.forward(data = {'a0':train_img})
+    G.backprop(0.01)
 
-canvas = np.concatenate([np.concatenate(p, axis = 0) for p in valid_img[:100].reshape(10, 10 , 28, 28, 1)], axis = 1)
-writer.add_image('read_img', canvas, 0)
+G.forward(data = {'a0':np.zeros((100,28*28))})
+#canvas = np.concatenate([np.concatenate(p, axis = 0) for p in oup.reshape(10, 10 , 28, 28, 1)], axis = 1)
+#writer.add_image('gen_img', canvas, 0)
+
+#canvas = np.concatenate([np.concatenate(p, axis = 0) for p in valid_img[:100].reshape(10, 10 , 28, 28, 1)], axis = 1)
+#writer.add_image('read_img', canvas, 0)
 
