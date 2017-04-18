54bae041cc010ac6e4a1124f395ae947798cb303
liuyujyyz <liuyujyyz@gmail.com>
2017-04-18 15:50:26 +0800

    0

--- core/dataprovider.py
+++ core/dataprovider.py
@@ -36,7 +36,7 @@ class DataProvider():
 
 class MNIST(DataProvider):
     def __init__(self, with_label = True):
-        a = pickle.load(open('data/mnist.pkl','rb'), encoding='latin1')
+        a = pickle.load(open('../data/mnist.pkl','rb'), encoding='latin1')
         train, valid, test = a
         self.train_img, self.train_label = train
         self.valid_img, self.valid_label = valid
@@ -49,14 +49,14 @@ class cifar10(DataProvider):
         data = []
         label = []
         for i in range(5):
-            a = pickle.load(open('data/cifar-10-patches-py/data_batch_{}'.format(i+1), 'rb'), encoding='latin1')
+            a = pickle.load(open('../data/cifar-10-patches-py/data_batch_{}'.format(i+1), 'rb'), encoding='latin1')
             data.append(a['data'])
             label.append(a['labels'])
         data = np.concatenate(data, axis = 0)
         label = np.concatenate(label, axis = 0)
         self.train_img = data
         self.train_label = label
-        a = pickle.load(open('data/cifar-10-patches-py/test_batch','rb'), encoding='latin1')
+        a = pickle.load(open('../data/cifar-10-patches-py/test_batch','rb'), encoding='latin1')
         self.test_img = a['data']
         self.test_label = a['labels']
         self.valid_img = a['data'][::10]
