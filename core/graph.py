9d31dbad00447610d8b617fb66050a1cf0c46b5c
liuyujyyz <liuyujyyz@gmail.com>
2017-04-18 15:47:21 +0800

    add conv

---
+++ core/graph.py
@@ -0,0 +1,141 @@
+from oprs import *
+#oprs dict name:[inputs, outputs, type, params]
+#params list name
+
+class node():
+    def __init__(self, name):
+        self.name = name
+        self.inputs = []
+        self.outputs = []
+        self.shape = None
+        self.value = None
+        self.delta = 0
+
+    def show(self):
+        print(self.name, self.inputs, self.outputs, self.value)
+
+class edge():
+    def __init__(self, name, inputs, outputs, opr_type, params):
+        func = func_dict[opr_type]
+        self.func = func(name, **params)
+        self.name = name
+        self.input = inputs
+        self.output = outputs
+
+    def valid(self, nodes):
+        for f in self.input:
+            if nodes[f].value is None:
+                return False
+        return True
+
+    def fw(self, nodes):
+        q = [nodes[f].value for f in self.input]
+        return self.func(q)
+
+    def bp(self, nodes, lr):
+        q = [nodes[f].value for f in self.input]
+        L = self.func.backprop(q, nodes[self.output].delta, lr)
+        for i in range(len(self.input)):
+            nodes[self.input[i]].delta += L[i]
+
+
+class graph():
+    def __init__(self, net_graph, debug = False):
+        self.nodes = {}
+        for key in net_graph['params']:
+            self.nodes[key] = node(key)
+        self.edges = {}
+        for key in net_graph['oprs']:
+            data = net_graph['oprs'][key]
+            self.edges[key] = edge(key, **data)
+            for p in data['inputs']:
+                self.nodes[p].outputs.append(key)
+            self.nodes[data['outputs']].inputs.append(key)
+        self.loss_var = self.nodes[net_graph['loss_var']]
+        for u in self.nodes:
+            self.nodes[u].show()
+        self.debug = debug
+
+    def load(self, filename):
+        data = pickle.load(open(filename, 'rb'))
+        self.nodes = data['nodes']
+        self.edges = data['edges']
+        self.loss_var = data['loss_var']
+
+    def save(self, filename):
+        pickle.dump({'nodes':self.nodes, 'edges':self.edges, 'loss_var':self.loss_var}, open(filename, 'wb'))
+        
+    def forward(self, data):
+        M = list(self.edges.keys())
+        for key in self.nodes:
+            self.nodes[key].value = None
+        for key in data:
+            self.nodes[key].value = data[key]
+
+        while M:
+            for opr in M:
+                if self.edges[opr].valid(self.nodes):
+                    val = self.edges[opr].fw(self.nodes)
+                    self.nodes[self.edges[opr].output].value = val
+                    M.remove(opr)
+        print(self.loss_var.value)
+
+    def backprop(self, lr):
+        count = {}
+        for key in self.nodes:
+            self.nodes[key].delta = 0
+            count[key] = 0
+        self.loss_var.delta = self.loss_var.value
+        M = list(self.edges.keys())
+        while M:
+            for opr in M:
+                outname = self.edges[opr].output
+                if count[outname] == len(self.nodes[outname].outputs):
+                    if self.debug:
+                        print(opr)
+                    self.edges[opr].bp(self.nodes, lr)
+                    for inname in self.edges[opr].input:
+                        count[inname] += 1
+                    M.remove(opr)
+
+class NetworkBuilder():
+    def __init__(self, name = 'HAHA'):
+        self.name = name
+        self.params = []
+        self.oprs = {}
+        self.time_stamp = 0
+        self.loss_var = None
+
+    def add_opr(self, name, inputs, outputs, opr_type, params):
+        assert not (name in self.oprs), 'duplicated name'
+        self.oprs[name] = {
+                'inputs': inputs,
+                'outputs': outputs,
+                'opr_type': opr_type,
+                'params': params,
+                }
+        for p in inputs:
+            if not (p in self.params):
+                self.params.append(p)
+        if not(outputs in self.params):
+            self.params.append(outputs)
+
+    def set_loss_var(self, loss):
+        self.loss_var = loss
+
+    def set_time_stamp(self, ts):
+        self.time_stamp = ts
+
+    def get_desc(self):
+        return {'params':self.params, 'oprs':self.oprs, 'loss_var':self.loss_var, 'time_stamp':self.time_stamp}
+    
+    def load(self, filename):
+        data = pickle.load(open(filename, 'rb'))
+        self.params = data['params']
+        self.oprs = data['oprs']
+        self.loss_var = data['loss_var']
+        self.time_stamp = data['time_stamp']
+
+    def save(self, filename):
+        desc = self.get_desc()
+        pickle.dump(desc, open(filename, 'wb'))
--- test_graph.py
+++ test_graph.py
@@ -1,7 +1,7 @@
 import pickle
-from monitor import *
-from dataprovider import *
-from graph import *
+from core.monitor import *
+from core.dataprovider import *
+from core.graph import *
 import numpy as np
 
 input_dims = 28 * 28
@@ -19,13 +19,17 @@ for i in range(4):
     NB.add_opr('fc%s'%i, ['a%s'%i], 'z%s'%i, 'FC', {'inp_size': ins[i], 'out_size':ins[i+1]})
     NB.add_opr('act%s'%i, ['z%s'%i], 'a%s'%(i+1), 'SIGMOID', {})
 
-NB.add_opr('sub1', ['a4', 'a0'], 'diff', 'SUB', {})
+NB.add_opr('reshape0', ['a4'], 'r0', 'RESHAPE', {'shape':(1,28,28)})
+NB.add_opr('conv', ['r0'], 'c0', 'CONV', {'inp_channel':1,'out_channel':1,'kernel_shape':(3,3),'stride':(1,1),'padding':(1,1)})
+NB.add_opr('reshape1', ['c0'], 'r1', 'RESHAPE', {'shape':(28*28,)})
+
+NB.add_opr('sub1', ['r1', 'a0'], 'diff', 'SUB', {})
 NB.add_opr('pow1', ['diff'], 'sq', 'POW', {'k':2})
 NB.add_opr('sum1', ['sq'], 'sum', 'SUM', {})
 NB.set_loss_var('sum')
 NB.set_time_stamp(start)
 net = NB.get_desc()
-G = graph(net)
+G = graph(net, debug = False)
 #writer = MonitorWriter('test_graph')
 #dp = MNIST(with_label = False)
 
@@ -33,9 +37,9 @@ G = graph(net)
 train_img = np.ones((1024, 28*28))
 for i in range(10):
     G.forward(data = {'a0':train_img})
-    G.backprop(0.01)
+    G.backprop(0.001)
 
-G.forward(data = {'a0':np.zeros((100,28*28))})
+G.forward(data = {'a0':np.ones((100,28*28))})
 #canvas = np.concatenate([np.concatenate(p, axis = 0) for p in oup.reshape(10, 10 , 28, 28, 1)], axis = 1)
 #writer.add_image('gen_img', canvas, 0)
 
