46e606d3b71c8d4bce7376cc44e5adf83cf9d2a2
liuyujyyz <liuyujyyz@gmail.com>
2017-10-20 21:59:42 +0800

    0

---
+++ cat_detector/train.sh
@@ -0,0 +1,5 @@
+python3 train.py --dataset ../data/data__president__1.pkl --max-iter 30
+
+python3 train.py --dataset ../data/data__president__1.pkl --lr 0.01 --cont train_log/model.pkl --max-iter 30
+
+python3 train.py --dataset ../data/data__president__1.pkl --lr 0.001 --cont train_log/model.pkl
