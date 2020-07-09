6f084361fd57bf4488f740c3353aeb12e1bfd169
liuyujyyz <liuyujyyz@gmail.com>
2020-07-09 18:25:50 +0800

    1

--- toys/basestruct.py
+++ toys/basestruct.py
@@ -14,6 +14,23 @@ class Person:
     def __init__(self):
         self.attrs = {attr.name: np.random.randint(10,100) for attr in BaseAttr}
 
+    @property
+    def level(self):
+        return 1
+
+    @property
+    def attack(self):
+        return self.attrs['base_attack'] + self.attrs['mat_attack'] * (self.level - 1)
+
+    @property
+    def defence(self):
+        return self.attrs['base_defence'] + self.attrs['mat_defence'] * (self.level - 1)
+
+    @property
+    def life(self):
+        return self.attrs['base_life'] + self.attrs['mat_life'] * (self.level - 1)
+
+
 if __name__ == '__main__':
     a = Person()
     print(a.attrs)
